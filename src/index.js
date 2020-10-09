c4e4bdc70e71a212086ad10784bd03f7a16ad44e
OlawaleJoseph <adedekojoseph@gmail.com>
2020-10-10 00:19:40 +0100

    create title scene

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import Phaser from 'phaser';
 import config from './config/config';
-import BootScene from './scenes/BootScene';
+import BootScene from './scenes/Boot';
 import PreloaderScene from './scenes/Preloader';
+import TitleScene from './scenes/Title';
 
 class Game extends Phaser.Game {
   constructor() {
     super(config);
     this.scene.add('Boot', BootScene);
     this.scene.add('Preloader', PreloaderScene);
+    this.scene.add('Title', TitleScene);
     this.scene.start('Boot');
   }
 }
---
+++ src/scenes/Title.js
@@ -0,0 +1,33 @@
+import Phaser from 'phaser';
+import config from '../config/config';
+import Button from '../components/Button';
+
+export default class TitleScene extends Phaser.Scene {
+  constructor() {
+    super('Title');
+  }
+
+  create() {
+    const averageScreenWidth = config.width / 2;
+    const averageScreenHeight = config.height / 2;
+    const button1 = 'button';
+    const button2 = 'button_hover';
+
+    this.gameButton = new Button(this, averageScreenWidth, averageScreenHeight - 150, button1, button2, 'Play', 'Game');
+
+    this.optionsButton = new Button(this, averageScreenWidth, averageScreenHeight - 50, button1, button2, 'Options', 'Options');
+
+    this.creditsButton = new Button(this, averageScreenWidth, averageScreenHeight + 50, button1, button2, 'Credits', 'Credits');
+
+    this.leaderBoardButton = new Button(this, averageScreenWidth, averageScreenHeight + 150, button1, button2, 'Rankings', 'Rankings');
+  }
+
+  centerButton(gameObject, offset = 0) {
+    Phaser.Display.Align.In.Center(
+      gameObject,
+      this.add.zone(
+        config.width / 2, config.height / 2 - offset * 100, config.width, config.height,
+      ),
+    );
+  }
+}
