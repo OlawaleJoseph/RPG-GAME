e6bcc6f0f055c1114a81f3f1c3b2ed99601b579c
OlawaleJoseph <adedekojoseph@gmail.com>
2020-10-11 23:35:31 +0100

    add player movement functionality

--- src/scenes/Game.js
+++ src/scenes/Game.js
@@ -35,30 +35,61 @@ export default class GameScene extends Phaser.Scene {
 
     this.anims.create({
       key: 'right',
-      frames: this.anims.generateFrameNumbers('player2', { frames: [4, 5, 6, 7] }),
+      frames: this.anims.generateFrameNumbers('hero', { frames: [4, 5, 6, 7] }),
       frameRate: 10,
       repeat: -1,
     });
 
     this.anims.create({
       key: 'left',
-      frames: this.anims.generateFrameNumbers('player2', { frames: [4, 5, 6, 7] }),
+      frames: this.anims.generateFrameNumbers('hero', { frames: [4, 5, 6, 7] }),
       frameRate: 10,
       repeat: -1,
     });
 
     this.anims.create({
       key: 'up',
-      frames: this.anims.generateFrameNumbers('player2', { frames: [12, 13, 14, 15] }),
+      frames: this.anims.generateFrameNumbers('hero', { frames: [12, 13, 14, 15] }),
       frameRate: 10,
       repeat: -1,
     });
     this.anims.create({
       key: 'down',
-      frames: this.anims.generateFrameNumbers('player2', { frames: [20, 21, 22, 23] }),
+      frames: this.anims.generateFrameNumbers('hero', { frames: [20, 21, 22, 23] }),
       frameRate: 10,
       repeat: -1,
     });
     this.physics.add.collider(this.player, obstacles);
+  }
+
+  update(time, delta) {
+    this.player.body.setVelocity(0);
+
+    // Horizontal movement
+    if (this.cursors.left.isDown) {
+      this.player.flipX = true;
+      this.player.body.setVelocityX(-80);
+    } else if (this.cursors.right.isDown) {
+      this.player.flipX = false;
+      this.player.body.setVelocityX(80);
+    }
+    // Vertical movement
+    if (this.cursors.up.isDown) {
+      this.player.body.setVelocityY(-80);
+    } else if (this.cursors.down.isDown) {
+      this.player.body.setVelocityY(80);
+    }
+
+    if (this.cursors.left.isDown) {
+      this.player.anims.play('left', true);
+    } else if (this.cursors.right.isDown) {
+      this.player.anims.play('right', true);
+    } else if (this.cursors.up.isDown) {
+      this.player.anims.play('up', true);
+    } else if (this.cursors.down.isDown) {
+      this.player.anims.play('down', true);
+    } else {
+      this.player.anims.stop();
+    }
   }
 }
