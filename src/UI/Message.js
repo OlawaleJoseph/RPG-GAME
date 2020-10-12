efc3dc85aefe9c4d5d684fc16a3247135910da4e
OlawaleJoseph <adedekojoseph@gmail.com>
2020-10-12 06:39:35 +0100

    display attack text

--- src/UI/Message.js
+++ src/UI/Message.js
@@ -8,10 +8,10 @@ export default class Message extends Phaser.GameObjects.Container {
     this.add(graphics);
     graphics.lineStyle(1, 0xffffff, 0.8);
     graphics.fillStyle(0x031f4c, 0.3);
-    graphics.strokeRect(-90, -15, 180, 30);
-    graphics.fillRect(-90, -15, 180, 30);
-    this.text = new Phaser.GameObjects.Text(scene, 0, 0, '', {
-      color: '#ffffff', align: 'center', fontSize: 13, wordWrap: { width: 160, useAdvancedWrap: true },
+    graphics.strokeRect(-90, -15, 180, 90);
+    graphics.fillRect(-90, -15, 180, 90);
+    this.text = new Phaser.GameObjects.Text(scene, 12, 17, '', {
+      color: '#ffffff', align: 'center', fontSize: 18, wordWrap: { width: 160, useAdvancedWrap: true },
     });
     this.add(this.text);
     this.text.setOrigin(0.5);
